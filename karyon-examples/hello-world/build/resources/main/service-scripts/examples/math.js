a8a3f501f54781f06a49a9216a1092d592e0aea3
Robert Christian <robertjchristian@gmail.com>
2013-04-21 14:42:14 -0700

    k

---
+++ karyon-examples/hello-world/build/resources/main/service-scripts/examples/math.js
@@ -0,0 +1,12 @@
+
+var add = function(a, b) {
+    println ();
+    println ("Hello from math.add()!");
+    return a + b;
+}
+
+var subtract = function(a, b) {
+    println ();
+    println ("Hello from math.subtract()!");
+    return a - b;
+}
