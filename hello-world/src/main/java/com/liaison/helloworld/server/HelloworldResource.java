7a298e5ded74f9a7cbeaaea7e0bcd6c039f388eb
Robert Christian <robertjchristian@gmail.com>
2013-05-05 11:52:34 -0700

    evolving dynamic services

--- hello-world/src/main/java/com/liaison/helloworld/server/HelloworldResource.java
+++ hello-world/src/main/java/com/liaison/helloworld/server/HelloworldResource.java
@@ -21,15 +21,12 @@ import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import javax.script.*;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.util.TreeMap;
 
 
 @Path("/hello")
