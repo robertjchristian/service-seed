cde795a4f55604a8b62df5eba888be28a187885a
rob <rob@Rob-MBP.local>
2013-05-10 16:58:54 -0700

    jmx

--- service-implementation/src/main/java/com/liaison/service/HelloworldResource.java
+++ service-implementation/src/main/java/com/liaison/service/HelloworldResource.java
@@ -16,10 +16,20 @@
 
 package com.liaison.service;
 
+import com.google.common.collect.Maps;
+import com.netflix.servo.DefaultMonitorRegistry;
+import com.netflix.servo.annotations.DataSourceType;
+import com.netflix.servo.annotations.Monitor;
+import com.netflix.servo.annotations.MonitorTags;
+import com.netflix.servo.tag.SortedTagList;
+import com.netflix.servo.tag.Tag;
+import com.netflix.servo.tag.TagList;
+import com.netflix.servo.BasicMonitorRegistry;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.netflix.servo.jmx.JmxMonitorRegistry;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -27,22 +37,56 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import com.netflix.servo.monitor.Monitors;
+
+import com.netflix.servo.publish.BasicMetricFilter;
+import com.netflix.servo.publish.CounterToRateMetricTransform;
+import com.netflix.servo.publish.FileMetricObserver;
+import com.netflix.servo.publish.JmxMetricPoller;
+import com.netflix.servo.publish.LocalJmxConnector;
+import com.netflix.servo.publish.MetricFilter;
+import com.netflix.servo.publish.MetricObserver;
+import com.netflix.servo.publish.MetricPoller;
+import com.netflix.servo.publish.PollRunnable;
+import com.netflix.servo.publish.PollScheduler;
+import com.netflix.servo.publish.PrefixMetricFilter;
+import com.netflix.servo.publish.RegexMetricFilter;
+
+import javax.management.ObjectName;
+import java.io.File;
+import java.util.NavigableMap;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 @Path("/hello")
 public class HelloworldResource {
 
     private static final Logger logger = LoggerFactory.getLogger(HelloworldResource.class);
 
+    @Monitor(name = "failureCounter", type = DataSourceType.COUNTER)
+    private final static AtomicInteger failureCounter = new AtomicInteger(0);
+
+    @Monitor(name = "serviceCallCounter", type = DataSourceType.COUNTER)
+    private final static AtomicInteger serviceCallCounter = new AtomicInteger(0);
+
+    public HelloworldResource() {
+       DefaultMonitorRegistry.getInstance().register(Monitors.newObjectMonitor(this));
+    }
 
     @Path("to/{name}")
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public Response helloTo(@PathParam("name") String name) {
+        serviceCallCounter.addAndGet(1);
         JSONObject response = new JSONObject();
         try {
             response.put("Message", "Hello " + name + " from Liaison");
             return Response.ok(response.toString()).build();
         } catch (JSONException e) {
+            failureCounter.addAndGet(1);
             logger.error("Error creating json response.", e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
@@ -51,11 +95,13 @@ public class HelloworldResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public Response hello() {
+        serviceCallCounter.addAndGet(1);
         JSONObject response = new JSONObject();
         try {
             response.put("Message", "Hello Friend!");
             return Response.ok(response.toString()).build();
         } catch (JSONException e) {
+            failureCounter.addAndGet(1);
             logger.error("Error creating json response.", e);
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
--- service-implementation/src/main/resources/dyn/scripts/math/math.js
+++ service-implementation/src/main/resources/dyn/scripts/math/math.js
@@ -2,7 +2,6 @@ function multiply (a, b) {
     return a*b;
 }
 
-
 function divide(x, y) {
     return x/y;
 }
